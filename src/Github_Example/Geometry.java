c7403e8df7514d92f8a170c6876f24695c309956
iusamasajjad <i.usamasajjad@gmail.com>
2024-04-21 23:40:01 +0500

    factory-method

---
+++ src/Github_Example/Geometry.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Github_Example;
+
+/**
+ *
+ * @author usamasajjad
+ */
+public abstract class Geometry {
+	public abstract Shape createShape();
+}
